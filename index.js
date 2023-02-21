2db77ea0aa0c70c8861ac1a56dfe901912309e77
ttdlyu <ttdlyu@163.com>
2023-02-21 08:28:37 +0800

    convert success

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 const fs = require("fs"),
       shiki = require("shiki"),
-      inputDir = ["html", "javascript", "java"];
+      inputDir = ["html", "js", "java"];
+const Output = "./dist"
+const baseDir = `./code`;
 shiki.getHighlighter({
-  theme: 'nord',
-  langs: ['javascript', 'java', 'html', "rust"]
+  theme: 'github-light',
+  langs: ['js', 'java', 'html', "rust"]
 }).then((highlighter) => {
   for (const i of inputDir) {
-    const baseDir = `./code/${i}/`;
-    for (const fileName of [...fs.readdirSync(baseDir)]) {
+    for (const fileName of [...fs.readdirSync(`${baseDir}/${i}/`)]) {
       if (fileName === "README.md") continue;
-      console.log(baseDir + fileName);
+      code2html(fileName, highlighter, i);
     }
   }
 });
-function code2html (fileName) {
-  fs.readFile("fileName", "utf-8", (error, data) => {
+function code2html (fileName, highlighter, cate) {
+  fs.readFile(`${baseDir}/${cate}/${fileName}`, "utf-8", (error, data) => {
     if (error) {
       console.error(error);
       return false;
     }
-    const out = highlighter.codeToHtml(data, {lang: 'html', theme: 'nord'});
-    // fs.mkdirSync('./convert/html/')
-    fs.writeFileSync('./convert/html/bfc.html',out, {
+    let out = highlighter.codeToHtml(data, {lang: cate, theme: 'github-light'});
+    if (!fs.existsSync(`${Output}/`)) {
+      fs.mkdirSync(`${Output}/`)
+    };
+    if (!fs.existsSync(`${Output}/${cate}/`)) {
+      fs.mkdirSync(`${Output}/${cate}/`)
+    };
+    out = `<link rel="stylesheet" href="../../assets/css/code.css">\r\n ${out}`
+    fileName = fileName.replace(`.${cate}`,'.html');
+    fs.writeFileSync(`${Output}/${cate}/${fileName}`,out, {
       encoding:"utf-8"
     });
   })
